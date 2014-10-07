6346de0f494fcb0d2c71b3e01cf94f43162a5124
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2014-10-07 23:23:57 +0300

    Migrated from the @RestController annotation to @Controller and @ResponseBody ones.

--- src/main/java/ua/org/javaday/spring/mvc/rest/MovieController.java
+++ src/main/java/ua/org/javaday/spring/mvc/rest/MovieController.java
@@ -1,23 +1,25 @@
 package ua.org.javaday.spring.mvc.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 import ua.org.javaday.spring.mvc.dao.MovieRepository;
 import ua.org.javaday.spring.mvc.domain.Movie;
 import ua.org.javaday.spring.mvc.rest.exception.MovieNotFoundException;
 
-@RestController
+@Controller
 @RequestMapping("/movies")
 public class MovieController {
     @Autowired
     private MovieRepository movieRepository;
 
-    @RequestMapping(value = "/{movieId}", method = RequestMethod.GET)
-    public Movie getMovie(@PathVariable int movieId) {
-        Movie movie = movieRepository.findById(movieId);
+    @ResponseBody
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Movie getMovie(@PathVariable int id) {
+        Movie movie = movieRepository.findById(id);
 
         if (movie != null) {
             return movie;
